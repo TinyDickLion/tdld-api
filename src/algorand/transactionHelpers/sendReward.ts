3956756527e41bcab3ff533a61fc2f149c9e44ce
TinyDickLion <tinydicklionsden@gmail.com>
2024-11-22 00:02:33 -0500

    ref

--- src/algorand/transactionHelpers/sendReward.ts
+++ src/algorand/transactionHelpers/sendReward.ts
@@ -32,7 +32,7 @@ export async function sendRewards(to: any, amount: any, assetId: any) {
         to,
         undefined, // closeRemainderTo
         undefined, // revocationTarget
-        BigInt(amount),
+        Number(amount),
         // @ts-ignore
         algosdk.encodeObj("Tiny Dick Lion's Den: Congrats! 🦁"),
         parseInt(assetId, 10), // Asset ID for ASA
