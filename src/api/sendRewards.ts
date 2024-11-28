87de335bf1aa8cede0b3d87cf5f4710c712ffaf2
TinyDickLion <tinydicklionsden@gmail.com>
2024-11-28 04:19:32 -0500

    ref: moved update to leaderboard to sendrewards helpers

--- src/api/sendRewards.ts
+++ src/api/sendRewards.ts
@@ -7,32 +7,13 @@ import { getRequiredBalance } from "../helpers/sendRewards/getRequiredBalance";
 import { calculateRewardAmount } from "../helpers/sendRewards/calculateRewardAmount";
 import { tokenDetails, TOKENS } from "../constants/tokendetails";
 import { readDataFile, writeDataFile } from "../helpers/readAndwritefiles";
-import { db } from "../config/firebase";
-import admin from "firebase-admin";
+import { updateLeaderboard } from "../helpers/sendRewards/UpdateLeaderboard";
 
 type SendRewardsResponse = { success: boolean; message?: string; txn?: any };
 
 const router = express.Router();
 
 const dataPath = path.join(__dirname, "../data/dailyparticipants.json");
-
-const getLeaderboardCollection = () => {
-  const currentDate = new Date();
-
-  // Find the closest Monday
-  const day = currentDate.getDay(); // Day of the week (0 = Sunday, 1 = Monday, etc.)
-  const daysSinceMonday = day === 0 ? 6 : day - 1; // Days since the last Monday
-  const mondayOfThisWeek = new Date(currentDate);
-  mondayOfThisWeek.setDate(currentDate.getDate() - daysSinceMonday);
-
-  // Calculate the week number based on Monday
-  const weekNumber = Math.floor(
-    mondayOfThisWeek.getTime() / (1000 * 60 * 60 * 24 * 7)
-  );
-
-  // Return a collection name that reflects the week number
-  return `weekly_leaderboard_${weekNumber}`;
-};
 
 // Route handler
 router.post<{}, SendRewardsResponse>(
@@ -104,25 +85,7 @@ router.post<{}, SendRewardsResponse>(
       }
 
       // Update leaderboard
-      const leaderboardCollection = getLeaderboardCollection();
-      const userRef = db.collection(leaderboardCollection).doc(to);
-
-      const userDoc = await userRef.get();
-
-      if (userDoc.exists) {
-        await userRef.update({
-          points: admin.firestore.FieldValue.increment(10),
-          updates: admin.firestore.FieldValue.arrayUnion(
-            new Date().toISOString()
-          ),
-        });
-      } else {
-        await userRef.set({
-          walletAddress: to,
-          points: 10,
-          updates: [new Date().toISOString()],
-        });
-      }
+      await updateLeaderboard(to);
 
       // Send reward in test mode
       res.json({
---
+++ src/helpers/sendRewards/UpdateLeaderboard.ts
@@ -0,0 +1,40 @@
+import { db } from "../../config/firebase";
+import admin from "firebase-admin";
+
+const getLeaderboardCollection = () => {
+  const currentDate = new Date();
+
+  // Find the closest Monday
+  const day = currentDate.getDay(); // Day of the week (0 = Sunday, 1 = Monday, etc.)
+  const daysSinceMonday = day === 0 ? 6 : day - 1; // Days since the last Monday
+  const mondayOfThisWeek = new Date(currentDate);
+  mondayOfThisWeek.setDate(currentDate.getDate() - daysSinceMonday);
+
+  // Calculate the week number based on Monday
+  const weekNumber = Math.floor(
+    mondayOfThisWeek.getTime() / (1000 * 60 * 60 * 24 * 7)
+  );
+
+  // Return a collection name that reflects the week number
+  return `weekly_leaderboard_${weekNumber}`;
+};
+
+export const updateLeaderboard = async (to: any) => {
+  const leaderboardCollection = getLeaderboardCollection();
+  const userRef = db.collection(leaderboardCollection).doc(to);
+
+  const userDoc = await userRef.get();
+
+  if (userDoc.exists) {
+    await userRef.update({
+      points: admin.firestore.FieldValue.increment(10),
+      updates: admin.firestore.FieldValue.arrayUnion(new Date().toISOString()),
+    });
+  } else {
+    await userRef.set({
+      walletAddress: to,
+      points: 10,
+      updates: [new Date().toISOString()],
+    });
+  }
+};
